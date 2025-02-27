d324352d00565472b4c50f4e43b02777eb61494e
AngelinaDash1990 <angelinadash1990@gmail.com>
2025-02-26 18:29:58 -0800

    EVERYTHING IS ABSOLUTELY 100% polished and DONE! :3

--- Angelina_ATM_Machine.DataAccess/Models/ClientModel.cs
+++ Angelina_ATM_Machine.DataAccess/Models/ClientModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,7 +11,8 @@ namespace Angelina_ATM_Machine.DataAccess.Models
     {
         public int ClientId { get; set; }
         public string AccountHolder { get; set; }
-        public int AccountNumber { get; set; } 
+        public int AccountNumber { get; set; }
+        [Required(ErrorMessage = "Yo dawg, you gotta enter a value! 🤑")]
         public decimal Balance { get; set; }
 
         public ClientModel(int clientId, int accountNumber, string accountHolder, decimal balance)
